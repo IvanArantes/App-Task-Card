917317fd66573e84c46a68786fbd490e058d380b
ivanarantes <ilvarantes@outlook.com>
2017-08-02 15:16:08 -0300

    First commit working

---
+++ app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {Task} from './model/task';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+
+})
+
+export class AppComponent{
+    private tasks: Task[] = [];
+    private currentTask = new Task(null, false);
+
+    addTask(num: number, str?: string) {
+        let task = new Task(this.currentTask.content, this.currentTask.completed);
+        this.tasks.push(task);
+        this.currentTask.content = null;
+    }
+}
